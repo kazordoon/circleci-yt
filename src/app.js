e43ba2620c5cda61fac8deccf05e43069d4a135e
Felipe Barros <felipetech@tuta.io>
2020-05-17 16:07:09 -0300

    chore: Add route to create a new product

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { uuid } from 'uuidv4';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+let products = [];
 
 app.get('/products', (req, res) => {
   // Listar todos os produtos
@@ -12,6 +15,29 @@ app.get('/products', (req, res) => {
 
 app.post('/products', (req, res) => {
   // Criar novo produto
+  const { code, description, buyPrice, sellPrice, tags } = req.body;
+
+  if (!code || !description || !buyPrice || !sellPrice || !tags) {
+    return res.status(406).json({ error: 'Incorrect request body.' });
+  }
+
+  let product = products.find(product => product.code === code);
+  if (product) {
+    return res.status(409).json({ error: 'This product already exists.' });
+  }
+
+  product = {
+    id: uuid(),
+    code,
+    description,
+    buyPrice,
+    sellPrice,
+    tags,
+    lovers: 0,
+  };
+  products.push(product);
+
+  return res.status(201).json({ product });
 });
 
 app.put('/products/:id', (req, res) => {
