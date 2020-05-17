728895fcabdbd59fb9781437d4a7e2f238cb2393
Felipe Barros <felipetech@tuta.io>
2020-05-17 16:08:37 -0300

    test: Add product creation tests

--- __tests__/product.test.js
+++ __tests__/product.test.js
@@ -1,3 +1,52 @@
 import request from 'supertest';
 
 import app from '../src/app';
+
+describe('Product', () => {
+  it('should create a new product', async () => {
+    const product = {
+      code: 1,
+      description: 'PC Gamer',
+      buyPrice: 1800,
+      sellPrice: 2000,
+      tags: ['PC'],
+    };
+    const response = await request(app)
+      .post('/products')
+      .send(product);
+
+    expect(response.status).toBe(201);
+    expect(response.body.product.id).toBeDefined();
+  });
+
+  it('should not to create a duplicated product', async () => {
+    const product = {
+      code: 1,
+      description: 'PC Gamer',
+      buyPrice: 1800,
+      sellPrice: 2000,
+      tags: ['PC'],
+    };
+    const response1 = await request(app)
+      .post('/products')
+      .send(product);
+
+    const response2 = await request(app)
+      .post('/products')
+      .send(product);
+
+    expect(response2.status).toBe(409);
+  });
+
+  it('should return the HTTP status code "406" trying to sending incorrect request body', async () => {
+    const invalidProduct = {
+      foo: 'bar',
+    };
+
+    const response = await request(app)
+      .post('/products')
+      .send(invalidProduct);
+
+    expect(response.status).toBe(406);
+  });
+});
