cfc95a70bdaa5dd0bda433089d4d67e6d84de290
Felipe Barros <felipetech@tuta.io>
2020-05-16 21:28:06 -0300

    chore: Add routes structure

--- src/app.js
+++ src/app.js
@@ -6,7 +6,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.json({ hello: 'world' }));
+app.get('/products', (req, res) => {
+  // Listar todos os produtos
+});
+
+app.post('/products', (req, res) => {
+  // Criar novo produto
+});
+
+app.put('/products/:id', (req, res) => {
+  // Atualizar produto pelo "id"
+});
+
+app.delete('/products/:code', (req, res) => {
+  // Excluir produto pelo "code"
+});
+
+app.post('/products/:code/love', (req, res) => {
+  // Desenvolver incremento de "love" por "code"
+});
+
+app.get('/products/:code', (req, res) => {
+  // Listar produtos pelo "code"
+});
 
 app.set('PORT', process.env.PORT || 3333);
 app.set('HOST', process.env.HOST || 'localhost');
